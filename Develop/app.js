a843ba9116f09e9414841fb82287e148f68fa66c
Shbibby <joshstillion@gmail.com>
2020-05-08 21:16:31 -0400

    employee classes created

--- Develop/app.js
+++ Develop/app.js
@@ -16,7 +16,28 @@ const render = require("./lib/htmlRenderer");
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+​class AskInfo {
+  constructor() {
+
+  }
+
+  askUser(title) {
+    if (title.toLowerCase() === "intern")
+    
+    inquirer
+      .prompt([
+      ])
+      .then(answers => {
+
+      })
+      .catch(error => {
+        if(error.isTtyError) {throw error};
+      });
+    //inquirer end
+  }
+
+}
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
